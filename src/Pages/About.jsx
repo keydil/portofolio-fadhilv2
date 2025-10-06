62dd8e69b65e81d63c3aeddd7867b72485c170e2
keydil <fadhilfirdausadha493@gmail.com>
2025-10-07 00:24:57 +0700

    update cv bos

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -308,7 +308,7 @@ const AboutPage = () => {
             </div>
 
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
-              <a href="/CV Terbaru - Fadhil FA.pdf" download className="w-full lg:w-auto">
+              <a href="/CV - Fadhil Firdaus Adha.pdf" download className="w-full lg:w-auto">
                 <button
                   data-aos="fade-up"
                   data-aos-duration="800"
